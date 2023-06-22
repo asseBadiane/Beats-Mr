945ba97881bf59a58b12c65592a5eec2a494e61e
asseBadiane <badianeasse99@gmail.com>
2023-06-22 13:26:32 +0000

    Implementer l'audio source one shot(jouer un son a démarrage)

--- audio_source_one_shot.py
+++ audio_source_one_shot.py
@@ -9,9 +9,16 @@ class AudioSourceOneShot(ThreadSource):
         self.chunk_nb_samples = 32
         self.wav_samples = wav_samples
         self.nb_wav_samples = len(wav_samples)
+        self.current_sample_index = 0
         self.buf = array('h', b"\x00\x00" * self.chunk_nb_samples)
 
 
     
     def get_bytes(self, *args, **kwargs):
+
+        for i in range(0, self.chunk_nb_samples):
+            if self.current_sample_index <= self.nb_wav_samples:
+                self.buf[i] = self.wav_samples[self.current_sample_index]
+            self.current_sample_index += 1
+
         return self.buf.tobytes()
