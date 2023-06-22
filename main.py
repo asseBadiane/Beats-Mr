14a95c5b0a2865037871ae1099d35f981dc8163d
asseBadiane <badianeasse99@gmail.com>
2023-06-22 10:44:26 +0000

    Nombre de tracks en mode dynamique

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from kivy.app import App
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.properties import ObjectProperty
 
 
+from track import TrackWidget
 from kivy.lang import Builder
 
 Builder.load_file("track.kv")
 
 
-
+NB_TRACKS = 4
 class MainWidget(RelativeLayout):
-    pass
+    tracks_layout = ObjectProperty()
 
 
+    def on_parent(self, widget, parent):
+        for i in range(0, NB_TRACKS):
+            self.tracks_layout.add_widget(TrackWidget())
 
 
 class MrBeatsApp(App):
--- track.py
+++ track.py
@@ -1,7 +1,15 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.togglebutton import ToggleButton
 
 
-
-
-class TrackWidget(BoxLayout):
+class TrackStepButton(ToggleButton):
     pass
+
+
+NB_STEPS_TRACKS = 16
+class TrackWidget(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(TrackWidget, self).__init__(**kwargs)
+        for i in range(0, NB_STEPS_TRACKS):
+            self.add_widget(TrackStepButton())
