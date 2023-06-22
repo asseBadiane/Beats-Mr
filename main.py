2ca73ac1ce97754d46be6ee4e0a72399d60b6c8d
asseBadiane <badianeasse99@gmail.com>
2023-06-22 10:59:58 +0000

    charger les differents Son avec SoundKitService et Modifier les nombre qui va dependre maintenant sur le nombre de son

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from kivy.app import App
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.properties import ObjectProperty
-
+from kivy.lang import Builder
 
 from track import TrackWidget
-from kivy.lang import Builder
+from sound_kit_service import  SoundKitService
 
 Builder.load_file("track.kv")
 
 
-NB_TRACKS = 4
+#NB_TRACKS = 4
 class MainWidget(RelativeLayout):
     tracks_layout = ObjectProperty()
 
+    def __init__(self, **kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+
+        self.sound_kit_service = SoundKitService()
 
     def on_parent(self, widget, parent):
-        for i in range(0, NB_TRACKS):
+        for i in range(0, self.sound_kit_service.get_nb_tracks()):
             self.tracks_layout.add_widget(TrackWidget())
 
 
---
+++ sound_kit_service.py
@@ -0,0 +1,26 @@
+
+
+class Sound:
+    def __init__(self, filename, displayname):
+        self.filename = filename
+        self.displayname = displayname
+
+class Soundkit:
+    sounds = ()
+
+    def get_nb_sound(self):
+        return  len(self.sounds)
+
+class SoundKit1(Soundkit):
+    sounds = (
+        Sound("sounds/kit1/kick.wav", "KICK"),
+        Sound("sounds/kit1/snare.wav", "SNARE"),
+        Sound("sounds/kit1/shaker.wav", "SHAKER"),
+        Sound("sounds/kit1/clap.wav", "CLAP"),
+    )
+
+class SoundKitService:
+    soundkit = SoundKit1()
+
+    def get_nb_tracks(self):
+        return self.soundkit.get_nb_sound()
