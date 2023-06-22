4e4a87bb895ec18906583798cebe17989885c505
asseBadiane <badianeasse99@gmail.com>
2023-06-22 13:46:55 +0000

    Gerer le button track pour jouer le son correspondant

--- main.py
+++ main.py
@@ -17,16 +17,17 @@ class MainWidget(RelativeLayout):
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         self.sound_kit_service = SoundKitService()
-        kik_sound = self.sound_kit_service.get_sound_index(0)
+
+        # kik_sound = self.sound_kit_service.get_sound_index(0)
 
         self.audio_engine = AudioEngine()
-        self.audio_engine.play_sound(kik_sound.samples)
+        # self.audio_engine.play_sound(kik_sound.samples)
 
 
     def on_parent(self, widget, parent):
         for i in range(0, self.sound_kit_service.get_nb_tracks()):
             sound = self.sound_kit_service.get_sound_index(i)
-            self.tracks_layout.add_widget(TrackWidget(sound))
+            self.tracks_layout.add_widget(TrackWidget(sound, self.audio_engine))
 
 
 class MrBeatsApp(App):
--- track.py
+++ track.py
@@ -11,12 +11,20 @@ class TrackSoundButton(Button):
 NB_STEPS_TRACKS = 16
 class TrackWidget(BoxLayout):
 
-    def __init__(self, sound, **kwargs):
+    def __init__(self, sound, audio_engine, **kwargs):
         super(TrackWidget, self).__init__(**kwargs)
 
         self.tracks_sound = TrackSoundButton()
         self.tracks_sound.text = sound.displayname
+        self.tracks_sound.on_press = self.on_play_sound_button_press
         self.add_widget(self.tracks_sound)
+
+        self.audio_engine = audio_engine
+        self.sound = sound
 
         for i in range(0, NB_STEPS_TRACKS):
             self.add_widget(TrackStepButton())
+
+    def on_play_sound_button_press(self):
+        # print("Sound!")
+        self.audio_engine.play_sound(self.sound.samples)
