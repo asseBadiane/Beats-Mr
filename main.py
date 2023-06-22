b4cf8ff1d660d1bf996e4882b344fcc68b8a2cf9
asseBadiane <badianeasse99@gmail.com>
2023-06-22 10:32:12 +0000

    Implementer les tracks

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from kivy.app import App
 from kivy.uix.relativelayout import RelativeLayout
 
 
+from kivy.lang import Builder
+
+Builder.load_file("track.kv")
+
+
 
 class MainWidget(RelativeLayout):
-    pass 
+    pass
+
+
 
 
 class MrBeatsApp(App):
