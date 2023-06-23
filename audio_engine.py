2932c2f989d4d31784da648b15d337659637d590
asseBadiane <badianeasse99@gmail.com>
2023-06-23 16:47:58 +0000

    Implementation du track audio  source

--- audio_engine.py
+++ audio_engine.py
@@ -1,6 +1,6 @@
 from audiostream import get_output
 from audio_source_one_shot import AudioSourceOneShot
-
+from audio_source_track import AudioSourceTrack
 
 class AudioEngine:
     CHANNELS = 1
@@ -20,3 +20,9 @@ class AudioEngine:
     
     def play_sound(self, wav_samples):
        self.audio_source_one_shot.set_wav_samples(wav_samples)
+
+    
+    def create_track(self, wav_samples, bpm):
+        source_track = AudioSourceTrack(self.output_stream, wav_samples, bpm, self.SAMPLES_RATE)
+        source_track.set_steps((1, 0, 0, 0))
+        source_track.start()
--- audio_source_track.py
+++ audio_source_track.py
@@ -7,41 +7,52 @@ class AudioSourceTrack(ThreadSource):
 
     def __init__(self, output_stream, wav_samples, bpm, sample_rate, *args, **kwargs):
         ThreadSource.__init__(self, output_stream, *args, **kwargs)
-        self.chunk_nb_samples = 32
+      
         self.wav_samples = wav_samples
 
         self.nb_wav_samples = len(wav_samples)
-        self.current_sample_index = 0
+        
+        self.current_steps_index = 0
         self.bpm = bpm
         self.sample_rate = sample_rate
-        self.buf = array('h', b"\x00\x00" * self.chunk_nb_samples)
-        self.compute_steps_nb_samples()
-
+        
+        self.compute_steps_nb_samples_and_alloc_buffer()
 
     def set_steps(self, steps):
+        if not len(steps) == self.steps:
+            self.current_steps_index = 0
         self.steps = steps
 
     
     def set_bpm(self, bpm):
         # bpm (beats per minute)
         self.bpm = bpm
-        self.compute_steps_nb_samples()
+        self.compute_steps_nb_samples_and_alloc_buffer()
         
     
-    def compute_steps_nb_samples(self):
-        self.steps_nb_samples = int(self.sample_rate * 15 / self.bpm) 
+    def compute_steps_nb_samples_and_alloc_buffer(self):
+        if not self.bpm == 0:
+            n = int(self.sample_rate * 15 / self.bpm) 
+            if self.steps_nb_samples != n:
+                self.steps_nb_samples = n
+                self.buf = array('h', b"\x00\x00" * self.steps_nb_samples)
 
 
     
     def get_bytes(self, *args, **kwargs):
 
-        if self.nb_wav_samples > 0:
-            for i in range(0, self.chunk_nb_samples):
-                if self.current_sample_index < self.nb_wav_samples:
-                    self.buf[i] = self.wav_samples[self.current_sample_index]
+        for i in range(0, self.steps_nb_samples):
+            if len(self.steps) > 0:
+                if self.steps[self.current_steps_index] == 1:
+                    # lancer mon son 
+                    self.buf[i] = self.wav_samples[i]
                 else:
                     self.buf[i] = 0
-                    
-                self.current_sample_index += 1
-
+            else:
+                self.buf[i] = 0
+        
+        self.current_steps_index += 1
+        if self.current_steps_index >= len(self.steps):
+            self.current_steps_index = 0
+            
         return self.buf.tobytes()
