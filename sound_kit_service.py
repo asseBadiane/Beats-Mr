6eeaf712240f1f747343ea3c4921edf3a71140ae
asseBadiane <badianeasse99@gmail.com>
2023-07-10 15:37:40 +0000

    Récupèrer les ressources et implementer les  images pour les steps buttons

--- sound_kit_service.py
+++ sound_kit_service.py
@@ -69,42 +69,19 @@ class SoundKit2(Soundkit):
 class SoundKit3(Soundkit):
     sounds = (
         Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
-        Sound("sounds/kit1/kick.wav", "KICK"),
+        Sound("sounds/kit1/snare.wav", "SNARE"),
+        Sound("sounds/kit1/shaker.wav", "SHAKER"),
+        Sound("sounds/kit1/clap.wav", "CLAP"),
+        Sound("sounds/kit1/pluck.wav", "PLUCK"),
+        Sound("sounds/kit1/effects.wav", "EFFECTS"),
+        Sound("sounds/kit1/bass.wav", "BASS"),
+        Sound("sounds/kit1/vocal_chop.wav", "VOCAL_CHOP"),
+
     )
 
 
 class SoundKitService:
-    soundkit = SoundKit3()
+    soundkit = SoundKit1()
 
     def get_nb_tracks(self):
         return self.soundkit.get_nb_sound()
