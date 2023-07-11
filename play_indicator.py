3382cd5c0b3bc14b19eee8306e98a04d01ef3f27
asseBadiane <badianeasse99@gmail.com>
2023-07-11 11:42:24 +0000

    Changer PlayindicatorButton en playIndicatorLight

--- play_indicator.py
+++ play_indicator.py
@@ -2,9 +2,13 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.button import Button
 from kivy.properties import NumericProperty
+from kivy.uix.image import Image
+from kivy.uix.widget import Widget
 
 
-class PlayIndicatorButton(ToggleButton):
+# class PlayIndicatorButton(ToggleButton):
+#     pass
+class PlayIndicatorLight(Image):
     pass
 
 
@@ -17,38 +21,33 @@ class PlayIndicatorWidget(BoxLayout):
     def set_nb_steps(self, nb_steps):
 
         if not nb_steps == self.nb_steps:
-            self.buttons = []
+            self.lights = []
             self.clear_widgets()
 
-            dummy_button = Button()
-            dummy_button.size_hint_x = None
-            dummy_button.width = self.left_align
-            dummy_button.disabled = True
-            self.add_widget(dummy_button)
+            dummy_widget = Widget()
+            dummy_widget.size_hint_x = None
+            dummy_widget.width = self.left_align
+            dummy_widget.disabled = True
+            self.add_widget(dummy_widget)
 
             for i in range(0, nb_steps):
-                button = PlayIndicatorButton()
+                light = PlayIndicatorLight()
+                light.source = "images/indicator_light_off.png"
 
-                button.disabled = True
-                # button.background_color = (0.5, .5, 1.0, 1.0)button = self.buttons[i]
-                # button.background_disabled_down = " "
-
-
-                self.buttons.append(button)
-                self.add_widget(button)
+                self.lights.append(light)
+                self.add_widget(light)
 
             self.nb_steps = nb_steps
 
 
     def current_step_index(self, index):
-        if index >= len(self.buttons):
+        if index >= len(self.lights):
             return
-        for i in range(0, len(self.buttons)):
-            button = self.buttons[i]
+        for i in range(0, len(self.lights)):
+            light = self.lights[i]
             if i == index:
                 
-                button.state = "down"
-                # button.background_color = (0.5, .5, 1.0, 1.0)
-                # button.background_disabled_down = " "
+                light.source = "images/indicator_light_on.png"
+               
             else:
-                button.state = "normal"
+                light.source = "images/indicator_light_off.png"
